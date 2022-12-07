31cfeb5721eafab262dbd8801247fbd80460d974
Bernhard Huber <bernihuber123@gmail.com>
2022-12-08 00:02:04 +0100

    add @Produces for specifying response content-type

--- web/src/main/java/org/huberb/pureko/resources/JavaEE8Resource.java
+++ web/src/main/java/org/huberb/pureko/resources/JavaEE8Resource.java
@@ -17,17 +17,20 @@ package org.huberb.pureko.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
  *
- * @author 
+ * @author
  */
 @Path("javaee8")
 public class JavaEE8Resource {
-    
+
     @GET
-    public Response ping(){
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response ping() {
         return Response
                 .ok("ping")
                 .build();

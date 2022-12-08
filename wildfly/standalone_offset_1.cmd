3dfc08f7c68be3364302f06b03f11103c56bee70
Bernhard Huber <bernihuber123@gmail.com>
2022-12-08 18:26:09 +0100

    allow additional cli args passing to standalone.cmd

--- wildfly/standalone_offset_1.cmd
+++ wildfly/standalone_offset_1.cmd
@@ -4,5 +4,5 @@ setlocal
 set BASEDIR=%~dp0
 set WILDFLY_DIR=%BASEDIR%\target\wildfly\wildfly-25.0.0.Final
 
-%WILDFLY_DIR%\bin\standalone.bat -Djboss.socket.binding.port-offset=1 
+%WILDFLY_DIR%\bin\standalone.bat -Djboss.socket.binding.port-offset=1 %*
 

e3e3b003b88fff87bfec6ac5223da00fa651c602
Bernhard Huber <bernihuber123@gmail.com>
2022-12-10 18:03:42 +0100

    fix css, and js webjars referencings

--- wildfly/setup_deployment_war_link.cmd
+++ wildfly/setup_deployment_war_link.cmd
@@ -4,7 +4,9 @@ setlocal
 set BASEDIR=%~dp0
 set SRC_WAR_DIR=%BASEDIR%\..\web\target\web-1.0-SNAPSHOT
 set DST_DEPLOYMENT_DIR=%BASEDIR%\target\wildfly\wildfly-25.0.0.Final\standalone\deployments
-set DST_DEPLOYMENT_WAR_FILE=%DST_DEPLOYMENT_DIR%\ko_pure_web.war
+
+@rem set DST_DEPLOYMENT_WAR_FILE=%DST_DEPLOYMENT_DIR%\ko_pure_web.war
+set DST_DEPLOYMENT_WAR_FILE=%DST_DEPLOYMENT_DIR%\ee8samples_web.war
 
 rmdir %DST_DEPLOYMENT_WAR_FILE%
 mklink /J %DST_DEPLOYMENT_WAR_FILE% %SRC_WAR_DIR%

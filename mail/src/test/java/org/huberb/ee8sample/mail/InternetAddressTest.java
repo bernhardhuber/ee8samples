1ae13ac855675be548e1b5b7420a423e56cb934b
Bernhard Huber <bernihuber123@gmail.com>
2022-12-18 01:06:21 +0100

    add InternetAddress tests

--- mail/src/test/java/org/huberb/ee8sample/mail/InternetAddressTest.java
+++ mail/src/test/java/org/huberb/ee8sample/mail/InternetAddressTest.java
@@ -12,7 +12,6 @@ import org.huberb.ee8sample.mail.MailsF.InternetAddressF;
 import org.huberb.ee8sample.mail.MailsF.InternetAddressF.Consumers;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -22,61 +21,106 @@ import org.junit.jupiter.params.provider.CsvSource;
  */
 public class InternetAddressTest {
 
-    Consumer<String[]> cInternetAddressF_1 = d -> {
-        String expectedAddress = d[0];
-        String inaddress = d[1];
+    static class G1 {
 
-        InternetAddressF iaf = new InternetAddressF();
-        iaf.consume(Consumers.address(inaddress)
-                .andThen(Consumers.validate())
-        );
+        static Consumer<String[]> cInternetAddressF_address = d -> {
+            String expectedAddress = d[0];
+            String inaddress = d[1];
 
-        InternetAddress ia = iaf.getInternetAddress();
-        assertEquals(expectedAddress, ia.toString());
-    };
-    Consumer<String[]> cInternetAddressF_2 = d -> {
-        String expectedAddress = d[0];
-        String inaddress = d[1];
+            InternetAddressF iaf = new InternetAddressF();
+            iaf.consume(Consumers.address(inaddress)
+                    .andThen(Consumers.validate())
+            );
 
-        InternetAddressF iaf = new InternetAddressF();
-        iaf.consume(Consumers.addressPersonalValidate(inaddress, null));
+            InternetAddress ia = iaf.getInternetAddress();
+            assertEquals(expectedAddress, ia.toString());
+        };
+        static Consumer<String[]> cInternetAddressFv2_address_null = d -> {
+            String expectedAddress = d[0];
+            String inaddress = d[1];
 
-        InternetAddress ia = iaf.getInternetAddress();
-        assertEquals(expectedAddress, ia.toString());
+            InternetAddressF iaf = new InternetAddressF();
+            iaf.consume(Consumers.addressPersonalValidate(inaddress, null));
 
-    };
-    Consumer<String[]> cInternetAddressBuilderF_1 = d -> {
-        String expectedAddress = d[0];
-        String inaddress = d[1];
+            InternetAddress ia = iaf.getInternetAddress();
+            assertEquals(expectedAddress, ia.toString());
 
-        InternetAddress ia = new InternetAddressBuilderF()
-                .address(inaddress)
-                .build();
+        };
+        static Consumer<String[]> cInternetAddressBuilderF_address = d -> {
+            String expectedAddress = d[0];
+            String inaddress = d[1];
 
-        assertEquals(expectedAddress, ia.toString());
-    };
-    Consumer<String[]> cInternetAddressBuilderTraditional_1 = d -> {
-        String expectedAddress = d[0];
-        String inaddress = d[1];
+            InternetAddress ia = new InternetAddressBuilderF()
+                    .address(inaddress)
+                    .build();
 
-        InternetAddress ia = new InternetAddressBuilderTraditional()
-                .address(inaddress)
-                .build();
+            assertEquals(expectedAddress, ia.toString());
+        };
+        static Consumer<String[]> cInternetAddressBuilderTraditional_address = d -> {
+            String expectedAddress = d[0];
+            String inaddress = d[1];
 
-        assertEquals(expectedAddress, ia.toString());
-    };
-    Consumer<String[]> cInternetAddressBuilderTraditional_2 = d -> {
-        String expectedAddress = d[0];
-        String inaddress = d[1];
-        String inpersonal = d[2];
+            InternetAddress ia = new InternetAddressBuilderTraditional()
+                    .address(inaddress)
+                    .build();
 
-        InternetAddress ia = new InternetAddressBuilderTraditional()
-                .address(inaddress)
-                .personal(inpersonal)
-                .build();
+            assertEquals(expectedAddress, ia.toString());
+        };
+    }
 
-        assertEquals(expectedAddress, ia.toString());
-    };
+    static class G2 {
+
+        static Consumer<String[]> cInternetAddressF_address_personal = d -> {
+            String expectedAddress = d[0];
+            String inaddress = d[1];
+            String inpersonal = d[2];
+
+            InternetAddressF iaf = new InternetAddressF();
+            iaf.consume(Consumers.address(inaddress)
+                    .andThen(Consumers.personal(inpersonal))
+                    .andThen(Consumers.validate())
+            );
+
+            InternetAddress ia = iaf.getInternetAddress();
+            assertEquals(expectedAddress, ia.toString());
+        };
+        static Consumer<String[]> cInternetAddressFv2_address_personal = d -> {
+            String expectedAddress = d[0];
+            String inaddress = d[1];
+            String inpersonal = d[2];
+
+            InternetAddressF iaf = new InternetAddressF();
+            iaf.consume(Consumers.addressPersonalValidate(inaddress, inpersonal));
+
+            InternetAddress ia = iaf.getInternetAddress();
+            assertEquals(expectedAddress, ia.toString());
+
+        };
+        static Consumer<String[]> cInternetAddressBuilderF_address_personal = d -> {
+            String expectedAddress = d[0];
+            String inaddress = d[1];
+            String inpersonal = d[2];
+
+            InternetAddress ia = new InternetAddressBuilderF()
+                    .address(inaddress)
+                    .personal(inpersonal)
+                    .build();
+
+            assertEquals(expectedAddress, ia.toString());
+        };
+        static Consumer<String[]> cInternetAddressBuilderTraditional_address_personal = d -> {
+            String expectedAddress = d[0];
+            String inaddress = d[1];
+            String inpersonal = d[2];
+
+            InternetAddress ia = new InternetAddressBuilderTraditional()
+                    .address(inaddress)
+                    .personal(inpersonal)
+                    .build();
+
+            assertEquals(expectedAddress, ia.toString());
+        };
+    }
 
     @ParameterizedTest
     @CsvSource(value = {
@@ -85,17 +129,23 @@ public class InternetAddressTest {
         "some@somehost.com,some@somehost.com",})
     public void given_address_then_create_an_InternetAddress(String expectedAddress, String inAddress) {
         String[] d = new String[]{expectedAddress, inAddress};
-        assertAll(
-                () -> cInternetAddressF_1.accept(d),
-                () -> cInternetAddressF_2.accept(d),
-                () -> cInternetAddressBuilderF_1.accept(d),
-                () -> cInternetAddressBuilderTraditional_1.accept(d));
+        assertAll(() -> G1.cInternetAddressF_address.accept(d),
+                () -> G1.cInternetAddressFv2_address_null.accept(d),
+                () -> G1.cInternetAddressBuilderF_address.accept(d),
+                () -> G1.cInternetAddressBuilderTraditional_address.accept(d));
     }
 
-    @Test
-    public void given_address_and_personal_then_create_an_InternetAddress() {
-        String[] d = new String[]{"Me <me@localhost>", "me@localhost", "Me"};
-        cInternetAddressBuilderTraditional_2.accept(d);
+    @ParameterizedTest
+    @CsvSource(value = {
+        "Me <me@localhost>, me@localhost, Me",
+         "You Too <you@somehost.com>,you@somehost.com,You Too",})
+    public void given_address_and_personal_then_create_an_InternetAddress(String expectedAddress, String inAddress, String inPersonal) {
+        String[] d = new String[]{expectedAddress, inAddress, inPersonal};
+        assertAll(() -> G2.cInternetAddressF_address_personal.accept(d),
+                () -> G2.cInternetAddressFv2_address_personal.accept(d),
+                () -> G2.cInternetAddressBuilderF_address_personal.accept(d),
+                () -> G2.cInternetAddressBuilderTraditional_address_personal.accept(d)
+        );
     }
 
 }

1def2d9297000b09ed992c0f774be259d319884e
Bernhard Huber <bernihuber123@gmail.com>
2022-12-18 01:08:36 +0100

    add InternetAddress tests

--- mail/src/test/java/org/huberb/ee8sample/mail/InternetAddressTest.java
+++ mail/src/test/java/org/huberb/ee8sample/mail/InternetAddressTest.java
@@ -1,6 +1,17 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright 2022 berni3.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.huberb.ee8sample.mail;
 
@@ -21,6 +32,9 @@ import org.junit.jupiter.params.provider.CsvSource;
  */
 public class InternetAddressTest {
 
+    /**
+     * Build internet address from address without personal.
+     */
     static class G1 {
 
         static Consumer<String[]> cInternetAddressF_address = d -> {
@@ -68,6 +82,9 @@ public class InternetAddressTest {
         };
     }
 
+    /**
+     * Build internet address from address plus personal.
+     */
     static class G2 {
 
         static Consumer<String[]> cInternetAddressF_address_personal = d -> {
@@ -138,7 +155,7 @@ public class InternetAddressTest {
     @ParameterizedTest
     @CsvSource(value = {
         "Me <me@localhost>, me@localhost, Me",
-         "You Too <you@somehost.com>,you@somehost.com,You Too",})
+        "You Too <you@somehost.com>,you@somehost.com,You Too",})
     public void given_address_and_personal_then_create_an_InternetAddress(String expectedAddress, String inAddress, String inPersonal) {
         String[] d = new String[]{expectedAddress, inAddress, inPersonal};
         assertAll(() -> G2.cInternetAddressF_address_personal.accept(d),
